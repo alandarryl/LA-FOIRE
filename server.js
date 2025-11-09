dba321f98d75e6c3d553b3b63daf425a097d4e16
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-11-09 11:28:54 +0100

    basic structure working

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./database/database');
+
+dotenv.config();
+
 const app = express();
+
+// connection a la base de données
+connectDB();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -9,7 +20,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-module.exports = app;
-
 
